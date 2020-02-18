34de27448393c12267e352065a0151e3a6cc4649
Sriharsha Ganti <sriharsha.ganti@minted.com>
2020-02-18 06:06:28 -0800

    Implemented multithreading

--- src/main/java/com/minted/urlvalidator/configuration/Configuration.java
+++ src/main/java/com/minted/urlvalidator/configuration/Configuration.java
@@ -1,7 +1,13 @@
 package com.minted.urlvalidator.configuration;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
+
+import com.minted.urlvalidator.RejectedExecutionHandlerImpl;
+
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
@@ -10,4 +16,18 @@ public class Configuration {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+	
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(100);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("URLValidator-");
+		executor.setRejectedExecutionHandler(new RejectedExecutionHandlerImpl());
+		executor.initialize();
+		return executor;
+	}
+
+
 }
--- src/main/java/com/minted/urlvalidator/service/URLValidatorService.java
+++ src/main/java/com/minted/urlvalidator/service/URLValidatorService.java
@@ -1,6 +1,6 @@
+
 package com.minted.urlvalidator.service;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,49 +11,44 @@ import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class URLValidatorService {
-
+	
+	
 	@Autowired
-	private RestTemplate restTemplate;
-
+	private EndpointCall endpointCall;
+	
 	@SuppressWarnings("unchecked")
-	public Map<String, String>  fxgJsonUrlValidator(List<String> urlList) throws InterruptedException, ExecutionException{
-		
-		final Map<String, String> resultMap = new HashMap<String, String>();
+	public Map<String,String>  fxgJsonUrlValidator(List<String> urlList) throws InterruptedException, ExecutionException{
+
+		Map<String,String> resultMap = new HashMap<String, String>();
+		List<CompletableFuture<ResponseEntity<String>>> allFutures = new ArrayList<>();
 		
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("cookie" , "renderEndpoint=rubric");
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		
 		for (String url : urlList) {
-			try {
-
-				ResponseEntity<String> responseEntity =  restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-
-			}catch(HttpServerErrorException serverErrorException) {
-				resultMap.put(url, serverErrorException.getStatusCode().toString());
-				System.out.println(url.concat(":").concat(serverErrorException.getStatusCode().toString()));
-
-			}catch(HttpClientErrorException clientErrorException) {
-				resultMap.put(url, clientErrorException.getStatusCode().toString());
-				System.out.println(url.concat(":").concat(clientErrorException.getStatusCode().toString()));
-			}
-
+			allFutures.add(endpointCall.callUrl(url, entity));
 		}
-
+		CompletableFuture.allOf(allFutures.toArray(new CompletableFuture[0])).join();
+		
+		for (int i = 0; i < urlList.size(); i++) {
+			try {
+				resultMap.put(urlList.get(i), allFutures.get(i).get().getStatusCode().toString());
+				System.out.println("URL: " + urlList.get(i) + ":   \t Status : " + allFutures.get(i).get().getStatusCode());
+			}catch(HttpServerErrorException serverException) {
+				resultMap.put(urlList.get(i), serverException.getStatusCode().toString());
+				System.out.println("URL: " + urlList.get(i) + ":   \t Status : " + serverException.getStatusCode().toString());
+			}catch(HttpClientErrorException clientException) {
+				resultMap.put(urlList.get(i), clientException.getStatusCode().toString());
+			}
+		}
 		return resultMap;
 	}
-
-
-
 }
